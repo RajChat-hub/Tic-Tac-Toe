338cedafbaf203471957181b04f4f482f06ca13b
RajChat-hub <chatterjeerajdeep124@gmail.com>
2024-10-16 17:25:03 +0530

    Delete Log.java

--- Log.java
+++
@@ -1,7 +0,0 @@
-public static void logTransaction(String message) {
-    try (FileWriter fw = new FileWriter("transaction_log.txt", true)) {
-        fw.write(message + "\n");
-    } catch (IOException e) {
-        System.out.println("Error logging transaction: " + e.getMessage());
-    }
-}
