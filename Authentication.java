a7fad7927f457106a3db4bc5f159a831d9b02efd
RajChat-hub <chatterjeerajdeep124@gmail.com>
2024-10-16 17:24:02 +0530

    Delete Authentication.java

--- Authentication.java
+++
@@ -1,9 +0,0 @@
-public static boolean authenticate() {
-    System.out.print("Enter username: ");
-    String username = scanner.nextLine();
-    System.out.print("Enter password: ");
-    String password = scanner.nextLine();
-
-    User user = new User("admin", "password123");
-    return user.login(username, password);
-}
